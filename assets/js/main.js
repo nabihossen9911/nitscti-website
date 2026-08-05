11f1d11b5c6288e52548b4eea2eaa001b730ad36
nabihossen9911 <nabihossen9911@gmail.com>
2026-08-05 16:51:11 +0600

    refactor: organize shared components and layout system

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,12 @@
 /*=========================================
   MOBILE MENU
 =========================================*/
-
 const menuToggle = document.querySelector(".menu-toggle");
-
 const navbar = document.querySelector(".navbar");
-
 if(menuToggle){
-
     menuToggle.addEventListener("click",()=>{
-
         navbar.classList.toggle("active");
-
     });
-
 }
 
 
@@ -24,13 +17,28 @@ if(menuToggle){
 const navLinks=document.querySelectorAll(".nav-list a");
 
 navLinks.forEach(link=>{
-
     link.addEventListener("click",()=>{
-
         navLinks.forEach(item=>item.classList.remove("active"));
-
         link.classList.add("active");
-
     });
+});
 
+
+/*=========================================
+    DARK / LIGHT MODE
+=========================================*/
+const themeToggle = document.getElementById("theme-toggle");
+/* Load saved theme */
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+/* Toggle Theme */
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
