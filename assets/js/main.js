c3b07db758002ec565e36e15c39d71055896a762
nabihossen9911 <nabihossen9911@gmail.com>
2026-08-10 22:22:49 +0600

    self top to bottom mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,17 +172,11 @@ SCROLLED HEADER
 const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
-
     if (window.scrollY > 50) {
-
         header.classList.add("scrolled");
-
     } else {
-
         header.classList.remove("scrolled");
-
     }
-
 });
 
 /*====================================================
@@ -190,25 +184,15 @@ SMOOTH NAVIGATION
 ====================================================*/
 
 navLinks.forEach(link => {
-
     link.addEventListener("click", function (e) {
-
         e.preventDefault();
-
         const target = document.querySelector(this.getAttribute("href"));
-
         if (target) {
-
             target.scrollIntoView({
-
                 behavior: "smooth"
-
             });
-
         }
-
     });
-
 });
 
 // ======================================================
